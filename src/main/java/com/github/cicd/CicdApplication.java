c415e66a3285bb1a638da42512901e040904aeab
rupeshh99 <rupeshhh94@gmail.com>
2024-01-25 18:56:04 +0530

    first commit-1

---
+++ src/main/java/com/github/cicd/CicdApplication.java
@@ -0,0 +1,27 @@
+package com.github.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	@GetMapping
+	public String getMessage(){
+		return "ATTABOY !! KEEP IT UP.";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
+//echo "# github-cicid" >> README.md
+//		git init
+//		git add README.md
+//		git commit -m "first commit"
+//		git branch -M main
+//		git remote add origin https://github.com/rupeshh99/github-cicid.git
+//		git push -u origin main
